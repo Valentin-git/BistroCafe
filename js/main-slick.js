5e1e88db38ceac7940609f0d8ce12cbd1526a39e
Valentin <valentin.zhdanovskiy@gmail.com>
2022-07-28 23:21:47 +0300

    4 commit

---
+++ js/main-slick.js
@@ -0,0 +1,28 @@
+$('.reviews-carousel').slick({
+  // centerMode: true,
+  // centerPadding: '60px',
+  slidesToShow: 1,
+  speed: 300,
+  easing: 'ease',
+  // autoplay: true,
+  // responsive: [
+  //   {
+  //     breakpoint: 768,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 3
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 480,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 1
+  //     }
+  //   }
+  // ]
+});
